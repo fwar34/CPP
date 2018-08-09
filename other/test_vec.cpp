daac5488f082e526869bc68c75ce32f7e309abc7
fwar34 <fwar34@126.com>
2018-08-09 12:34:41 +0800

    add cond and test vector

---
+++ other/test_vec.cpp
@@ -0,0 +1,26 @@
+/*************************************************************************
+    > File Name: test_vec.cpp
+    > Author: Feng
+    > Created Time: Wed 09 May 2018 04:52:48 PM CST
+    > Content: 
+ ************************************************************************/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> a;
+    vector<int> b;
+    a.push_back(1);
+    a.push_back(2);
+    a.push_back(3);
+    b.push_back(1);
+    b.push_back(2);
+    b.push_back(3);
+
+    cout << a == b << endl;
+    return 0;
+}
